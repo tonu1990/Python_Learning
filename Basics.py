99f73589e091c351f4becd47b7ddfa1e42258dd8
tonu1990 <58003234+tonu1990@users.noreply.github.com>
2026-06-20 00:00:40 +0000

    Adding Python basics .py file

---
+++ Basics.py
@@ -0,0 +1,18 @@
+# this is a way to add comment using #
+
+''' This is another way to add comments using Triple quotes- string literal comment'''
+
+string_a = "This is sample string"
+print(string_a)
+
+string_b = """This is multiple line string .
+This is second line of string.
+String ends at third line"""
+print(string_b)
+
+# Assigning multiple values to multiple variables
+a, b, c , d = 5, 3.2, 'Hello' , "Tonu"
+print(a)
+print(b)
+print(c)
+print(d)
