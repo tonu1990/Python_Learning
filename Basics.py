350836c3d955af7d24f2ccfeac39ca73ea098c8c
tonu1990 <58003234+tonu1990@users.noreply.github.com>
2026-06-21 20:45:46 +0000

    OOps Notes

--- Basics.py
+++ Basics.py
@@ -16,3 +16,46 @@ print(a)
 print(b)
 print(c)
 print(d)
+
+# concatenate two different non compatiable data types and error handling 
+
+try :
+    print (a+c)
+
+     
+except Exception as e:
+    print(e)
+
+#immutability 
+
+print(id(a))
+i= 7
+print (id(i))
+i= i+1 
+a= a+3
+
+ 
+print(id(a))
+print (id(i))
+
+# adding numbers of different types 
+a = 14 + 4j
+b= True
+c= 3.14
+d = 7
+
+print(a+b)
+print(a+c)
+print(a+d)
+print(b+c)
+print(b+d)
+print(c+d)
+
+# Check cached integer range
+for i in range(-10, 10):
+    obj_id = id(i)
+    print(f"Value: {i}, Address: {obj_id}")
+
+#size of int
+import sys
+print(sys.getsizeof(d))
